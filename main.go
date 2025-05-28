7754fd0257785a2f75026eb6e513e937811b44ec
udehlee <ritzeepita@gmail.com>
2025-05-28 19:59:04 +0100

    Set up zerolog logger with timestamp, initialized DB and eBay client

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Udehlee/alert-Me/api"
+	"github.com/Udehlee/alert-Me/client"
+	"github.com/Udehlee/alert-Me/db/db"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	log := zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
+	Client := client.NewEbayClient(os.Getenv("ACCESS_TOKEN"))
+
+	db, err := db.InitConnectDB()
+	if err != nil {
+		log.Err(err).Msg("failed to connect to database")
+
+	}
+
+	h := api.NewHandler(db, Client, log)
+}
