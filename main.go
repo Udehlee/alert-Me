5ac4c9f51088f2dd4961e90d61d87292ceae9aa3
udehlee <ritzeepita@gmail.com>
2025-06-07 21:34:57 +0100

    initializes rabbitMQ connection

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/Udehlee/alert-Me/api"
 	"github.com/Udehlee/alert-Me/db/db"
+	"github.com/Udehlee/alert-Me/pkg/rabbitMQ"
+	"github.com/Udehlee/alert-Me/pkg/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
@@ -13,13 +15,23 @@ func main() {
 	r := gin.Default()
 	log := zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
 
-	db, err := db.InitConnectDB()
+	dbConn, err := db.InitConnectDB()
 	if err != nil {
-		log.Err(err).Msg("failed to connect to database")
-
+		log.Fatal().Err(err).Msg("failed to connect to database")
 	}
 
-	h := api.NewHandler(db, log)
+	rb, err := rabbitMQ.ConnectRabbitMQ(dbConn)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to connect to RabbitMQ")
+	}
+
+	go rb.Consumer("product-url", utils.ScrapeProduct)
+
+	h := api.NewHandler(log, &rb)
 	h.RegisterRoutes(r)
+
+	if err := r.Run(":8000"); err != nil {
+		log.Fatal().Err(err).Msg("failed to start server")
+	}
 
 }
