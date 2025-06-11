6fceff743c766e6b9d38cb926d88d6804bd0b014
udehlee <ritzeepita@gmail.com>
2025-06-11 21:16:24 +0100

    change queue name from scrape_product_queue to product_url_queue to match consumer queue name

--- api/api.go
+++ api/api.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 
 	"github.com/Udehlee/alert-Me/models"
-	"github.com/Udehlee/alert-Me/pkg/rabbitMQ"
+	"github.com/Udehlee/alert-Me/pkg/rabbitmq"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
 
 type Handler struct {
-	rabbit *rabbitMQ.RabbitMQ
+	rabbit *rabbitmq.RabbitMQ
 	log    zerolog.Logger
 }
 
-func NewHandler(log zerolog.Logger, rabbit *rabbitMQ.RabbitMQ) *Handler {
+func NewHandler(log zerolog.Logger, rabbit *rabbitmq.RabbitMQ) *Handler {
 	return &Handler{
 		rabbit: rabbit,
 		log:    log,
@@ -40,11 +40,11 @@ func (h *Handler) SubmitProduct(c *gin.Context) {
 
 	body, err := json.Marshal(reqProduct)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to serialize request"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to marshal reqProduct request"})
 		return
 	}
 
-	err = h.rabbit.PublishToQueue("scrape_product_queue", body)
+	err = h.rabbit.PublishToQueue("product_url_queue", body)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to publish URL to queue"})
 		return
