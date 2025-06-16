6060ed40c49cfe4d6e224d9a6ab03dfcf5f9c51a
udehlee <ritzeepita@gmail.com>
2025-06-16 06:56:23 +0100

    remove users table and change from slelectedProduct to product for clarity

--- db/migrations/20250526193546_create_users_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    email TEXT NOT NULL UNIQUE,
-    pass_word TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
--- db/migrations/20250526193732_create_selected_products_table.up.sql
+++
@@ -1,9 +0,0 @@
-  CREATE TABLE selectedProducts (
-    id INT SERIAL PRIMARY KEY,
-    name_ TEXT NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    product_url TEXT NOT NULL,
-    status_ TEXT DEFAULT 'pending',
-    created_at TIMESTAMP DEFAULT NOW(),
-    last_checked TIMESTAMP DEFAULT NOW()
-    );
