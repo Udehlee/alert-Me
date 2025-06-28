5d4b92a12656e1405eaba2cc7c7f56865c3d0d0f
udehlee <ritzeepita@gmail.com>
2025-06-28 14:35:04 +0100

    update LastChecked to *time.Time to  allow null values

--- models/models.go
+++ models/models.go
@@ -3,13 +3,13 @@ package models
 import "time"
 
 type Product struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name_"`
-	Price       float64   `json:"price"`
-	URL         string    `json:"product_url"`
-	Status      string    `json:"status_"`
-	CreatedAt   time.Time `json:"created_at"`
-	LastChecked time.Time `json:"last_checked"`
+	ID          int        `json:"id"`
+	Name        string     `json:"name_"`
+	Price       float64    `json:"price"`
+	URL         string     `json:"product_url"`
+	Status      string     `json:"status_"`
+	CreatedAt   time.Time  `json:"created_at"`
+	LastChecked *time.Time `json:"last_checked"`
 }
 
 type UrlRequest struct {
