bc8019919fdae461e02fcb535242e41c124c443b
udehlee <ritzeepita@gmail.com>
2025-06-28 14:17:05 +0100

    re-add migration files to fix missing  migrations

--- internals/db/migrations/000001_create_products_table.up.sql
+++ internals/db/migrations/000001_create_products_table.up.sql
@@ -1,9 +1,9 @@
-  CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name_ TEXT NOT NULL,
-    price DOUBLE PRECISION NOT NULL,
-    product_url TEXT NOT NULL,
-    status_ TEXT DEFAULT 'pending',
-    created_at TIMESTAMP DEFAULT NOW(),
-    last_checked TIMESTAMP DEFAULT NOW()
-    );
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name_ TEXT NOT NULL,
+  price DOUBLE PRECISION NOT NULL,
+  product_url TEXT NOT NULL,
+  status_ TEXT DEFAULT 'pending',
+  created_at TIMESTAMP DEFAULT NOW(),
+  last_checked TIMESTAMP
+);
